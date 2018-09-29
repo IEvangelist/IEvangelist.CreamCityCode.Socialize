623db96f3bf35e0f3a84cbb10dbd3d4b67ab029c
David Pine <david.pine.7@gmail.com>
2018-09-29 16:30:10 -0500

    Ah, hagit status! CSS Grid...

--- IEvangelist.CreamCityCode.Socialize/Models/ShareViewModel.cs
+++ IEvangelist.CreamCityCode.Socialize/Models/ShareViewModel.cs
@@ -8,6 +8,16 @@ namespace IEvangelist.CreamCityCode.Socialize.Models
 
         public Uri ImageUrl => IsSingleImage ? AllImageUrls[0] : null;
         
-        public Uri[] AllImageUrls { get; set; }
+        internal Uri[] AllImageUrls { get; set; }
+
+        public ArraySegment<Uri> FirstQuarter => new ArraySegment<Uri>(AllImageUrls, 0, QuarterLength);
+
+        public ArraySegment<Uri> SecondQuarter => new ArraySegment<Uri>(AllImageUrls, QuarterLength, QuarterLength);
+
+        public ArraySegment<Uri> ThirdQuarter => new ArraySegment<Uri>(AllImageUrls, QuarterLength * 2, QuarterLength);
+
+        public ArraySegment<Uri> FourthQuarter => new ArraySegment<Uri>(AllImageUrls, QuarterLength * 3, QuarterLength);
+
+        private int QuarterLength => AllImageUrls.Length / 4;
     }
 }
